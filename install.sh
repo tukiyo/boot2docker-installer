04e49947928d76557761e30d527777c471ac6ac5
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 08:59:12 +0900

    update

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ EOF
 mkfs.ext4 -L boot2docker-data /dev/sda2
 
 ## mount
-grep sda2 /proc/partitions || sleep 5
-mount /dev/sda2 /mnt/sda2
+mount /dev/sda2 /mnt/sda2 || (sleep 5; mount /dev/sda2 /mnt/sda2)
 
 B2D=/mnt/sda2/var/lib/boot2docker
 mkdir -p $B2D
