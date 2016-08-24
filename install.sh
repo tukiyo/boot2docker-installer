8b1bb64b5d4339f5d336310350496c313a98acd8
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 08:25:30 +0900

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+#-----------------------------------
+# create boot2docker-data partition
+#-----------------------------------
 dd if=/dev/sr0 of=/dev/sda bs=1M
 /usr/local/sbin/partprobe
-
 fdisk /dev/sda <<EOF
 n
 p
@@ -10,6 +12,25 @@ p
 
 w
 EOF
-
 mkfs.ext4 -L boot2docker-data /dev/sda2
+
+#-----------------------------------
+# mount
+#-----------------------------------
+PERSIST=/mnt/sda2/var/lib/boot2docker/
+mkdir -p $PERSIST
+mount /dev/sda2 $PERSIST
+
+#-----------------------------------
+# profile
+#-----------------------------------
+cat $PERSIST/profile <<EOF
+ulimit -n 65535
+EOF
+
+#-----------------------------------
+# bootlocal.sh
+#-----------------------------------
+cat $PERSIST/bootlocal.sh <<EOF
 export TZ="JST-9"
+EOF
