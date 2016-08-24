b9362bace02648bb89f2684a87c7ff5d0237f747
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 08:50:17 +0900

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#-----------------------------------
-# create boot2docker-data partition
-#-----------------------------------
+## install
 dd if=/dev/sr0 of=/dev/sda bs=1M
 /usr/local/sbin/partprobe /dev/sda
+
+## create
 fdisk /dev/sda <<EOF
 n
 p
@@ -13,5 +13,19 @@ p
 w
 EOF
 mkfs.ext4 -L boot2docker-data /dev/sda2
-sh settings.sh
-#reboot
+
+## mount
+mount /dev/sda2 /mnt/sda2
+
+B2D=/mnt/sda2/var/lib/boot2docker
+mkdir -p $B2D
+
+cat > $B2D/profile <<EOF
+ulimit -n 65535
+EOF
+
+cat > $B2D/bootlocal.sh <<EOF
+export TZ="JST-9"
+hostname boot2docker2
+EOF
+chmod +x $B2D/bootlocal.sh
