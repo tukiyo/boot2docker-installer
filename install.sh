36532fe7a56698fd9e32dc53786a9553a3e2c146
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 09:17:35 +0900

    update

--- install.sh
+++ install.sh
@@ -3,29 +3,36 @@ dd if=/dev/sr0 of=/dev/sda bs=1M
 /usr/local/sbin/partprobe /dev/sda
 
 ## create
-fdisk /dev/sda <<EOF
-n
-p
-2
-
-
-
-w
-EOF
+(echo n; echo p; echo 2; echo; echo; echo; echo; echo w) | fdisk /dev/sda
 mkfs.ext4 -L boot2docker-data /dev/sda2
 
 ## mount
 mount /dev/sda2 /mnt/sda2 || (sleep 5; mount /dev/sda2 /mnt/sda2)
-
 B2D=/mnt/sda2/var/lib/boot2docker
 mkdir -p $B2D
 
+## profile
 cat > $B2D/profile <<EOF
 ulimit -n 65535
 EOF
 
+## startup script
 cat > $B2D/bootlocal.sh <<EOF
 hostname boot2docker2
-echo JST-9 > /etc/localtime
+ip addr add 10.0.2.16/24 dev eth0
+ip route add default via 10.0.2.1
+echo nameserver 8.8.8.8 >> /etc/resolv.conf
+echo 'TZ="JST-9"' > /etc/sysconfig/timezone
+mount -o bind /var/lib/boot2docker/home /home/docker
+mount -o bind /var/lib/boot2docker/root /root
 EOF
 chmod +x $B2D/bootlocal.sh
+
+## sshd
+mkdir $B2D/ssh
+cat > $B2D/ssh/sshd_config <<EOF
+#UseDNS no
+#Port 22
+#PasswordAuthentication no
+#PermitRootLogin no
+EOF
