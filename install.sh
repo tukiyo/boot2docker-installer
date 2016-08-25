3217bb11ead05455e4c6376b9eab831b2d3c9370
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 09:30:28 +0900

    update

--- install.sh
+++ install.sh
@@ -1,30 +1,36 @@
+#------------------------------------------------------------------------
+# CONFIG
+NAMESERVER="8.8.8.8"
+IPADDR="10.0.2.16/24"
+ETH=eth0
+GATEWAY="10.0.2.1"
+DISK=sda
+#------------------------------------------------------------------------
+
 ## install
-dd if=/dev/sr0 of=/dev/sda bs=1M
-/usr/local/sbin/partprobe /dev/sda
+dd if=/dev/sr0 of=/dev/${DISK} bs=1M
+/usr/local/sbin/partprobe /dev/${DISK}
 
 ## create
-(echo n; echo p; echo 2; echo; echo; echo; echo w) | fdisk /dev/sda
-mkfs.ext4 -L boot2docker-data /dev/sda2
+(echo n; echo p; echo 2; echo; echo; echo w) | fdisk /dev/${DISK}
+mkfs.ext4 -L boot2docker-data /dev/${DISK}2
 
 ## mount
-mount /dev/sda2 /mnt/sda2 || (sleep 5; mount /dev/sda2 /mnt/sda2)
-B2D=/mnt/sda2/var/lib/boot2docker
+mount /dev/${DISK}2 /mnt/${DISK}2 || (sleep 5; mount /dev/${DISK}2 /mnt/${DISK}2)
+B2D=/mnt/${DISK}2/var/lib/boot2docker
 mkdir -p $B2D
-
-## profile
-cat > $B2D/profile <<EOF
-ulimit -n 65535
-EOF
 
 ## startup script
 cat > $B2D/bootlocal.sh <<EOF
 hostname boot2docker2
-ip addr add 10.0.2.16/24 dev eth0
-ip route add default via 10.0.2.1
-echo nameserver 8.8.8.8 >> /etc/resolv.conf
+ip addr add $IPADDR dev $ETH
+ip route add default via $GATEWAY
+echo nameserver $NAMESERVER >> /etc/resolv.conf
 echo 'TZ="JST-9"' > /etc/sysconfig/timezone
 mount -o bind /var/lib/boot2docker/home /home/docker
 mount -o bind /var/lib/boot2docker/root /root
+mount -t tmpfs tmpfs /tmp
+ulimit -n 65535
 EOF
 chmod +x $B2D/bootlocal.sh
 
@@ -35,6 +41,8 @@ cat > $B2D/ssh/sshd_config <<EOF
 #Port 22
 #PasswordAuthentication no
 #PermitRootLogin no
+#X11Forwarding yes
+#X11DisplayOffset 10
 EOF
 
 ## data folders
