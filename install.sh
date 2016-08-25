f9c6f22a7684d047572abc1c726a2a7766ba0903
tukiyo3 <tukiyo3@gmail.com>
2016-08-25 10:49:10 +0900

    update

--- install.sh
+++ install.sh
@@ -2,24 +2,20 @@
 set -eu
 
 # CONFIG----------------------------------------------------------------
+HOSTNAME="b2d"
+#IPADDR="10.0.2.16/24"
+#GATEWAY="10.0.2.1"
+#ETH=eth0
+DISK=sda
+#-----------------------------------------------------------------------
 #GITHUB_USER="tukiyo"
 if [ $# -eq 1 ];then
     GITHUB_USER=$1
 else
-    echo "To wget https://github.com/Username.keys "
+    echo "To wget https://github.com/Username.keys ,"
     echo -n "enter GitHub's Username : "
     read GITHUB_USER
 fi
-#
-HOSTNAME="b2d"
-#
-IPADDR="10.0.2.16/24"
-GATEWAY="10.0.2.1"
-ETH=eth0
-NAMESERVER="8.8.8.8"
-#
-DISK=sda
-#-----------------------------------------------------------------------
 
 ## install
 dd if=/dev/sr0 of=/dev/${DISK} bs=1M
@@ -38,17 +34,28 @@ mkdir -p $B2D
 cat > $B2D/bootlocal.sh <<EOF
 #!/bin/sh
 set -x
-
 hostname $HOSTNAME
-ip addr add $IPADDR dev $ETH
-ip route add default via $GATEWAY
-echo nameserver $NAMESERVER >> /etc/resolv.conf
 echo 'TZ="JST-9"' > /etc/sysconfig/timezone
 mount -o bind /var/lib/boot2docker/home /home/docker
 mount -o bind /var/lib/boot2docker/root /root
 mount -t tmpfs tmpfs /mnt/sda2/tmp
+if [ -e $B2D/network.sh ];then
+    sh $B2D/network.sh
+fi
 EOF
 chmod +x $B2D/bootlocal.sh
+
+## network settings.
+#cat > $B2D/network.sh <<EOF
+#ip addr add $IPADDR dev $ETH
+#ip route add default via $GATEWAY
+#EOF
+
+## template file.
+## see: /etc/init.d/docker
+cat > $B2D/profile <<EOF
+#DOCKER_HOST='-H tcp://0.0.0.0:2375'
+EOF
 
 ## sshd
 mkdir $B2D/ssh
@@ -62,6 +69,10 @@ EOF
 ## data folders
 mkdir -p $B2D/home/.ssh $B2D/root/.ssh
 wget https://github.com/$GITHUB_USER.keys -O $B2D/root/.ssh/authorized_keys
-cp $B2D/root/.ssh/authorized_keys $B2D/home/.ssh/authorized_keys
+cp $B2D/root/.ssh/authorized_keys $B2D/home/.ssh/
 chmod 600 $B2D/home/.ssh $B2D/root/.ssh
 chown -R docker:staff $B2D/home
+
+
+## done
+echo "install finished. please reboot."
--- ip.sh
+++
@@ -1,5 +0,0 @@
-IPADDR=$(awk -F '=' '/^IPADDR/{print $2}' ip.conf)
-GATEWAY=$(awk -F '=' '/^GATEWAY/{print $2}' ip.conf)
-
-echo ip addr add $IPADDR dev eth0
-echo ip route add default via $GATEWAY
